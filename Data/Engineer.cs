b6f51f214f93281195fb76ddd32beb595bb8d675
Vladimir Vorobiev <vladimir.vorobiev@intelex.com>
2023-11-07 15:22:06 -0500

    Add project files.

---
+++ Data/Engineer.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor.Azure.CosmosDB.Demo.Data
+{
+    public class Engineer
+    {
+        public Guid id { get; set; }
+        public string? Name { get; set; }
+        public string? Country { get; set; }
+        public string? Specialty { get; set; }
+    }
+
+}
---
+++ Data/EngineerService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.Cosmos;
+using System.ComponentModel;
+using static System.Net.WebRequestMethods;
+using Container = Microsoft.Azure.Cosmos.Container;
+
+namespace Blazor.Azure.CosmosDB.Demo.Data
+{
+    public class EngineerService
+    {
+        private readonly string CosmosDbConnectionString = "AccountEndpoint=https://azure-dev-vvv-cosmos-db.documents.azure.com:443/;AccountKey=YLfiOx57pZMim9nxj7UvfAZEPLX8wJEjNFAggXOh86FWiW7yEgedfBQ5TETmeFKzAMovL2o4hOOxACDbB9O7XA==;";
+        private readonly string CosmosDbName = "Contractors";
+        private readonly string CosmosDbContainerName = "Engineers";
+        public EngineerService() { }
+
+        private Container GetContainerClient()
+        {
+            var cosmosDbClient = new CosmosClient(CosmosDbConnectionString);
+            var container = cosmosDbClient.GetContainer(CosmosDbName, CosmosDbContainerName);
+            return container;
+        }
+
+        public async Task AddEngineer(Engineer engineer)
+        {
+            try
+            {
+                engineer.id = Guid.NewGuid();
+                var container = GetContainerClient();
+                var response = await container.CreateItemAsync(engineer, new PartitionKey(engineer.id.ToString()));
+                Console.WriteLine(response.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString().Trim();
+            }
+        }
+
+        public async Task DeleteEngineer(string? id, string? partitionKey)
+        {
+            try
+            {
+                var container = GetContainerClient();
+                var response = await container.DeleteItemAsync<Engineer>(id, new PartitionKey(partitionKey));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Delete", ex);
+            }
+        }
+    }
+
+}
+
+///
